86a4b9d43811cf339bd3cc51ca6678b44514e434
Taha Soomro <tahasoomro2017@gmail.com>
2024-12-26 07:57:20 +0500

    Your commit message

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,24 +1,23 @@
 @extends('layouts.main')
-
 @section('main-container')
-    <div class="container mt-5" style="color: white;">
-        <h2 class="text-center">Products</h2>
-        <div class="row">
-            @foreach($products as $product)
-                <div class="col-md-4 mb-4">
-                    <div class="card">
-                        <img src="{{ asset('storage/' . $product->image) }}" class="card-img-top" alt="{{ $product->name }}">
-                        <div class="card-body text-center">
-                            <h5 class="card-title">{{ $product->name }}</h5>
-                            <p class="card-text">{{ $product->description }}</p>
-                            <p class="card-text"><strong>Price:</strong> ${{ $product->price }}</p>
-                            <p class="card-text"><strong>Category:</strong> {{ $product->category }}</p>
-                            <p class="card-text"><strong>Genre:</strong> {{ $product->genre }}</p>
-                            <button class="btn btn-primary">Add to Cart</button>
-                        </div>
-                    </div>
+<div class="container mt-5" style="color: white;">
+    <h2 class="text-center">Products</h2>
+    <div class="row">
+        @foreach($products as $product)
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="{{ asset('storage/' . $product->image) }}" class="card-img-top" alt="{{ $product->name }}">
+                <div class="card-body text-center">
+                    <h5 class="card-title">{{ $product->name }}</h5>
+                    <p class="card-text">Category: {{ $product->category }}</p>
+                    <p class="card-text">Genre: {{ $product->genre }}</p>
+                    <p class="card-text">{{ $product->description }}</p>
+                    <p class="card-text">Price: ${{ $product->price }}</p>
+                    <button class="btn btn-primary">Add to Cart</button>
                 </div>
-            @endforeach
+            </div>
         </div>
+        @endforeach
     </div>
+</div>
 @endsection
